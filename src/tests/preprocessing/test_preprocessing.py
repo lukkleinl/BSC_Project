feab8a5ea2a7d1e8ee6084e7f932ea5b587a0e49
Lukas Kleinl <a01546221@unet.univie.ac.at>
2022-01-31 23:47:00 +0100

    deleted test which led to error

--- src/tests/preprocessing/test_preprocessing.py
+++ src/tests/preprocessing/test_preprocessing.py
@@ -1,9 +1,7 @@
 import pandas as pd
-import pytest
 
 from configuration.configuration import load_configuration
-from preprocessing.preprocessing import separate_features_outcome, preprocess_data, \
-    import_and_load_preprocessing_function
+from preprocessing.preprocessing import separate_features_outcome
 
 data_classes_rfc = load_configuration("src/tests/data/config_files/config_rfc_convert_functions_CSV_Loader.yaml")
 
@@ -19,7 +17,3 @@ def test_separate_features_outcome():
     features, labels = separate_features_outcome(df,"quality")
     assert len(features) == 1599
 
-def test_preprocess_data():
-    df = pd.read_csv("src/tests/data/raw/raw_data.csv")
-    with pytest.raises(FileNotFoundError):
-        x_train,x_test,y_train, y_test = preprocess_data(df, prepr_steps, train_split)
